8bff7619e905c7b42622b6596d8a44c3bc5323d9
Rodro <rodrigo.merino@mulesoft.com>
2019-06-19 11:39:20 -0300

    Revert MULE-16540: AuthorizationCodeState not updated on certain circumstances

--- src/main/java/org/mule/service/oauth/internal/DefaultAuthorizationCodeOAuthDancer.java
+++ src/main/java/org/mule/service/oauth/internal/DefaultAuthorizationCodeOAuthDancer.java
@@ -9,14 +9,11 @@ package org.mule.service.oauth.internal;
 import static java.lang.String.format;
 import static java.lang.String.valueOf;
 import static java.lang.Thread.currentThread;
-import static java.util.Collections.emptyMap;
 import static java.util.Collections.singleton;
 import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 import static org.mule.runtime.api.metadata.MediaType.ANY;
 import static org.mule.runtime.api.metadata.MediaType.parse;
-import static org.mule.runtime.api.util.MultiMap.emptyMultiMap;
-import static org.mule.runtime.api.util.Preconditions.checkArgument;
 import static org.mule.runtime.core.api.util.ClassUtils.withContextClassLoader;
 import static org.mule.runtime.core.api.util.StringUtils.isBlank;
 import static org.mule.runtime.http.api.HttpConstants.HttpStatus.BAD_REQUEST;
@@ -72,7 +69,6 @@ import org.mule.runtime.http.api.server.async.ResponseStatusCallback;
 import org.mule.runtime.oauth.api.AuthorizationCodeOAuthDancer;
 import org.mule.runtime.oauth.api.AuthorizationCodeRequest;
 import org.mule.runtime.oauth.api.builder.AuthorizationCodeDanceCallbackContext;
-import org.mule.runtime.oauth.api.builder.AuthorizationCodeListener;
 import org.mule.runtime.oauth.api.exception.RequestAuthenticationException;
 import org.mule.runtime.oauth.api.exception.TokenNotFoundException;
 import org.mule.runtime.oauth.api.exception.TokenUrlResponseException;
@@ -89,12 +85,10 @@ import org.slf4j.Logger;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.locks.Lock;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
@@ -124,7 +118,6 @@ public class DefaultAuthorizationCodeOAuthDancer extends AbstractOAuthDancer imp
 
   private final Function<AuthorizationCodeRequest, AuthorizationCodeDanceCallbackContext> beforeDanceCallback;
   private final BiConsumer<AuthorizationCodeDanceCallbackContext, ResourceOwnerOAuthContext> afterDanceCallback;
-  private final List<AuthorizationCodeListener> listeners;
 
   private RequestHandlerManager redirectUrlHandlerManager;
   private RequestHandlerManager localAuthorizationUrlHandlerManager;
@@ -141,8 +134,7 @@ public class DefaultAuthorizationCodeOAuthDancer extends AbstractOAuthDancer imp
                                              LockFactory lockProvider, Map<String, DefaultResourceOwnerOAuthContext> tokensStore,
                                              HttpClient httpClient, MuleExpressionLanguage expressionEvaluator,
                                              Function<AuthorizationCodeRequest, AuthorizationCodeDanceCallbackContext> beforeDanceCallback,
-                                             BiConsumer<AuthorizationCodeDanceCallbackContext, ResourceOwnerOAuthContext> afterDanceCallback,
-                                             List<AuthorizationCodeListener> listeners) {
+                                             BiConsumer<AuthorizationCodeDanceCallbackContext, ResourceOwnerOAuthContext> afterDanceCallback) {
     super(clientId, clientSecret, tokenUrl, encoding, scopes, encodeClientCredentialsInBody, responseAccessTokenExpr,
           responseRefreshTokenExpr,
           responseExpiresInExpr, customParametersExtractorsExprs, resourceOwnerIdTransformer, lockProvider, tokensStore,
@@ -159,12 +151,6 @@ public class DefaultAuthorizationCodeOAuthDancer extends AbstractOAuthDancer imp
 
     this.beforeDanceCallback = beforeDanceCallback;
     this.afterDanceCallback = afterDanceCallback;
-
-    if (listeners != null) {
-      this.listeners = new CopyOnWriteArrayList<>(listeners);
-    } else {
-      this.listeners = new CopyOnWriteArrayList<>();
-    }
   }
 
   @Override
@@ -174,18 +160,6 @@ public class DefaultAuthorizationCodeOAuthDancer extends AbstractOAuthDancer imp
       localAuthorizationUrlHandlerManager =
           addRequestHandler(s, GET, localAuthorizationUrlPath, createLocalAuthorizationUrlListener());
     });
-  }
-
-  @Override
-  public void addListener(AuthorizationCodeListener listener) {
-    checkArgument(listener != null, "Cannot add a null listener");
-    listeners.add(listener);
-  }
-
-  @Override
-  public void removeListener(AuthorizationCodeListener listener) {
-    checkArgument(listener != null, "Cannot remove a null listener");
-    listeners.remove(listener);
   }
 
   private static RequestHandlerManager addRequestHandler(HttpServer server, Method method, String path,
@@ -322,7 +296,6 @@ public class DefaultAuthorizationCodeOAuthDancer extends AbstractOAuthDancer imp
                 updateResourceOwnerState(resourceOwnerOAuthContext, stateDecoder.decodeOriginalState(), tokenResponse);
                 updateResourceOwnerOAuthContext(resourceOwnerOAuthContext);
 
-                listeners.forEach(l -> l.onAuthorizationCompleted(resourceOwnerOAuthContext));
                 afterDanceCallback.accept(beforeCallbackContext, resourceOwnerOAuthContext);
 
                 sendResponse(stateDecoder, responseCallback, OK, "Successfully retrieved access token",
@@ -510,11 +483,6 @@ public class DefaultAuthorizationCodeOAuthDancer extends AbstractOAuthDancer imp
 
   @Override
   public CompletableFuture<Void> refreshToken(String resourceOwner) {
-    return refreshToken(resourceOwner, false);
-  }
-
-  @Override
-  public CompletableFuture<Void> refreshToken(String resourceOwner, boolean useQueryParameters) {
     if (LOGGER.isDebugEnabled()) {
       LOGGER.debug("Executing refresh token for user " + resourceOwner);
     }
@@ -553,7 +521,6 @@ public class DefaultAuthorizationCodeOAuthDancer extends AbstractOAuthDancer imp
                   }
                   updateResourceOwnerState(resourceOwnerOAuthContext, null, tokenResponse);
                   updateResourceOwnerOAuthContext(resourceOwnerOAuthContext);
-                  listeners.forEach(l -> l.onTokenRefreshed(resourceOwnerOAuthContext));
                 });
               } finally {
                 lock.unlock();
