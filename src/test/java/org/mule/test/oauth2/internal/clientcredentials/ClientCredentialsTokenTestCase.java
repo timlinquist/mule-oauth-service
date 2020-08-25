9cec7bcfe536e98bb7121d41ce77f590cb9650ae
Ezequiel Werner <eze210@gmail.com>
2020-08-25 15:16:06 -0300

    MULE-18687: Inconsistence between dancer state and refresh token value in OAuth context (#83) (#84)
    (cherry picked from commit 67770692a647b43fc40dabb46333e7a93cb57944)

--- src/test/java/org/mule/test/oauth2/internal/clientcredentials/ClientCredentialsTokenTestCase.java
+++ src/test/java/org/mule/test/oauth2/internal/clientcredentials/ClientCredentialsTokenTestCase.java
@@ -29,7 +29,9 @@ import static org.mule.runtime.http.api.HttpHeaders.Names.AUTHORIZATION;
 import static org.mule.runtime.oauth.api.builder.ClientCredentialsLocation.BASIC_AUTH_HEADER;
 import static org.mule.runtime.oauth.api.builder.ClientCredentialsLocation.BODY;
 import static org.mule.runtime.oauth.api.builder.ClientCredentialsLocation.QUERY_PARAMS;
+import static org.mule.runtime.oauth.api.state.DancerState.HAS_TOKEN;
 import static org.mule.service.oauth.internal.ResourceOwnerOAuthContextUtils.isDancerStateNoToken;
+import static org.mule.runtime.oauth.api.state.ResourceOwnerOAuthContext.DEFAULT_RESOURCE_OWNER_ID;
 
 import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.api.util.MultiMap;
@@ -38,6 +40,8 @@ import org.mule.runtime.http.api.domain.message.request.HttpRequest;
 import org.mule.runtime.oauth.api.ClientCredentialsOAuthDancer;
 import org.mule.runtime.oauth.api.builder.OAuthClientCredentialsDancerBuilder;
 import org.mule.runtime.oauth.api.state.ResourceOwnerOAuthContext;
+import org.mule.runtime.oauth.api.state.ResourceOwnerOAuthContext;
+import org.mule.runtime.oauth.api.state.ResourceOwnerOAuthContextWithRefreshState;
 import org.mule.test.oauth.AbstractOAuthTestCase;
 
 import java.net.URI;
@@ -326,7 +330,7 @@ public class ClientCredentialsTokenTestCase extends AbstractOAuthTestCase {
   }
 
   @Test
-  public void exeptionOnTokenRequest() throws Exception {
+  public void exceptionOnTokenRequest() throws Exception {
     final IllegalStateException thrown = new IllegalStateException();
     when(httpClient.sendAsync(any(), any())).thenThrow(thrown);
 
@@ -336,9 +340,34 @@ public class ClientCredentialsTokenTestCase extends AbstractOAuthTestCase {
 
     expected.expect(sameInstance(thrown));
     try {
-      ClientCredentialsOAuthDancer minimalDancer = startDancer(builder);
+      startDancer(builder);
     } finally {
       assertThat(isDancerStateNoToken(tokensStore.get("default")), is(true));
+      assertThat(tokensStore.get("default").getAccessToken(), is(nullValue()));
+    }
+  }
+
+  @Test
+  public void exceptionOnTokenRequestWithPreviouslyStoredToken() throws Exception {
+    final IllegalStateException thrown = new IllegalStateException();
+    when(httpClient.sendAsync(any(), any())).thenThrow(thrown);
+
+    final Map<String, ResourceOwnerOAuthContextWithRefreshState> tokensStore = new HashMap<>();
+    ResourceOwnerOAuthContextWithRefreshState previousState =
+        new ResourceOwnerOAuthContextWithRefreshState(DEFAULT_RESOURCE_OWNER_ID);
+    previousState.setAccessToken("PreviousToken");
+    previousState.setDancerState(HAS_TOKEN);
+    tokensStore.put(DEFAULT_RESOURCE_OWNER_ID, previousState);
+
+    final OAuthClientCredentialsDancerBuilder builder = baseClientCredentialsDancerBuilder(tokensStore);
+    builder.tokenUrl("http://host/token");
+
+    expected.expect(sameInstance(thrown));
+    try {
+      startDancer(builder);
+    } finally {
+      assertThat(isDancerStateNoToken(tokensStore.get("default")), is(true));
+      assertThat(tokensStore.get(DEFAULT_RESOURCE_OWNER_ID).getAccessToken(), is(nullValue()));
     }
   }
 
